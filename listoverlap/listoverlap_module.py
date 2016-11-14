e82b4564a944e02afe809e19b17ecc2e6eabacaa
agnes-kabaly <alicet.woods@gmail.com>
2016-11-14 23:56:40 +0100

    Update listoverlap_module.py

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,10 +1,13 @@
 def listoverlap(list1, list2):
-    return
-
+    result = []
+    result_overlap = [i for i in set(list1) if i in list2]
+    for element in result_overlap:
+      if element not in result:
+        result.append(element)
+    return result
 
 def main():
     return
-
 
 if __name__ == '__main__':
     main()
