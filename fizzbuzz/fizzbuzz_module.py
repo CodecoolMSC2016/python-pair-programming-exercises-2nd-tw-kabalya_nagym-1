ec5a901c8b51820e083b9b887fbb77f80f515e02
agnes-kabaly <alicet.woods@gmail.com>
2016-11-14 15:11:45 +0100

    Fizz is ready! X_X

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,15 @@
 def fizzbuzz(number):
-    return
-
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
+    if number % 3 == 0:
+        return 'Fizz'
+    if number % 5 == 0:
+        return 'Buzz'
+    return number
 
 def main():
+    for i in range(0,101):
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
